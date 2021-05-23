ff9bd44e906d1dab6e2e59de0e327e7bafb3b12c
Seonjun19 <parkseonjun1119@naver.com>
2021-05-23 23:55:55 +0900

    gam

---
+++ gam2project/blog/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import redirect, render
+from .models import Blog
+from django.utils import timezone
+# Create your views here.
+def home(request):
+    blogs = Blog.objects
+    return render(request, 'home.html', {'blogs':blogs})
+
+def detail(request, blog_id):
+    blog_detail = Blog.objects.get(id = blog_id)
+    return render(request, 'detail.html', {'blog': blog_detail})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    blog = Blog()
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+    return redirect('/blog/' + str(blog.id))
+
+def delete(request, blog_id):
+    Blog.objects.get(id = blog_id).delete()
+    return redirect('/')
+
+def edit(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    return render(request, 'edit.html', {'blog':blog})
+
+def update(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    blog.title = request.POST.get('title')
+    blog.body = request.POST.get('body')
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+    return redirect('/blog/' + str(blog.id))
+
+def index(request):
+    return render(request, 'index.html')
+
+def introduce(request):
+    return render(request, 'introduce.html')
+
+def history(request):
+    return render(request, 'history.html')
+
+def contact_me(request):
+    return render(request, 'contact_me.html')
+
+def favorite(request):
+    return render(request, 'favorite.html')
---
+++ gam2project/gam2project/urls.py
@@ -0,0 +1,34 @@
+"""gam2project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+import blog.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', blog.views.home, name='home'),
+    path('blog/<int:blog_id>/', blog.views.detail, name='detail'),
+    path('blog/new/', blog.views.new, name='new'),
+    path('blog/create/', blog.views.create, name='create'),
+    path('blog/<int:blog_id>/delete', blog.views.delete, name='delete'),
+    path('blog/<int:blog_id>/edit', blog.views.edit, name='edit'),
+    path('blog/<int:blog_id>/update', blog.views.update, name='update'),
+    path('blog/index/', blog.views.index, name='index'),
+    path('blog/introduce/', blog.views.introduce, name='introduce'),
+    path('blog/history/', blog.views.history, name='history'),
+    path('blog/contact_me/', blog.views.contact_me, name='contact_me'),
+    path('blog/favoirte/', blog.views.favorite, name='favorite'),
+]
